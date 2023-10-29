1d63642a9270c96e08e7f6846f45b961c9cf06b3
drull <pcaetanop@protonmail.com>
2023-10-29 19:08:30 -0300

    Fixing devices list

--- airplayConcept/ContentView.swift
+++ airplayConcept/ContentView.swift
@@ -7,81 +7,29 @@
 
 import SwiftUI
 
-struct Device: Identifiable, Equatable {
-    var id = UUID()
-    var name: String
-    var location: String
-    var selected: Bool
-    var song: String
-    var artist: String
-    
-}
+
 
 struct ContentView: View {
     
-    @State private var devices: [Device] = [
-        Device(name: "homepod.fill", location: "Bathroom", selected: false, song: "Shape of You", artist: "Ed Sheeran"),
-        Device(name: "homepodmini.fill", location: "Kitchen", selected: true, song: "Blinding Lights", artist: "The Weeknd"),
-        Device(name: "homepod.2.fill", location: "Living Room", selected: true, song: "Uptown Funk", artist: "Mark Ronson ft. Bruno Mars"),
-        Device(name: "homepodmini.2.fill", location: "Master Bedroom + Hallway", selected: false, song: "Watermelon Sugar", artist: "Harry Styles"),
-        Device(name: "homepodmini.fill", location: "Garage", selected: false, song: "Happy", artist: "Pharrell Williams"),
-    ]
-    
     var body: some View {
-            VStack{
-                SongView()
-                MoveButton()
-                    .frame(height: 140)
-                
-                HStack{
-                    Text("Speakers and TV's")
-                        .fontWeight(.semibold)
-                        .foregroundStyle(Color.white)
-                    Spacer()
-                }
-                .padding(.horizontal, 20)
-                .background(Color("BackgroundColor"))
-                
-            }
-            List(devices) { device in
-                HStack(alignment: .center) {
-                    Image(systemName: device.name)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 35, height: 35) 
-                        .foregroundStyle(Color.white)
-                        .padding(.trailing, 3)
-                    
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(device.location)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(Color.white)
-                        
-                        Text("\(device.song) - \(device.artist)")
-                            .font(.subheadline)
-                            .foregroundStyle(Color.white)
-                            .lineLimit(1)
-                            .truncationMode(.tail)
-                    }
-                    
-                    Spacer()
-                    
-                    if device.selected {
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundStyle(Color.white)
-                            .font(.title2)
-                    } else {
-                        Image(systemName: "circle")
-                            .foregroundStyle(Color.white)
-                            .font(.title2)
-                    }
-                }
-                .padding(.trailing, 10)
-                .listRowBackground(Color.accentColor)
-                
-            }
+        VStack{
+            SongView()
+            MoveButton()
+                .frame(height: 140)
             
+            HStack{
+                Text("Speakers and TV's")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(Color.white)
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            .background(Color("BackgroundColor"))
+            DevicesListView()
+            .padding(.horizontal, 10)
+                .scrollContentBackground(.hidden)
         }
+    }
         
 }
 #Preview {
