383f1eabb25aee8f1a25f0d83d25defd6bd1620d
drull <pcaetanop@protonmail.com>
2023-10-29 14:24:59 -0300

    Initial Commit

---
+++ airplayConcept/airplayConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  airplayConceptApp.swift
+//  airplayConcept
+//
+//  Created by Pedro Caetano on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct airplayConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
